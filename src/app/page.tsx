23da807476cf332fba16a7c283fe164846fdb808
void-57 <ruddhaani60@gmail.com>
2025-03-21 00:14:07 +0530

    Add navigation links for crop and resize image functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="p-4">
+      <Link href="/crop" className="mr-4 text-blue-500 hover:underline">
+        Crop Image
+      </Link>
+      <Link href="/resize" className="text-blue-500 hover:underline">
+        Resize Image
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
