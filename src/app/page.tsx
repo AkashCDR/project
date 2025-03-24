a0380799ee04cae3bb7d67015bdac928f8fffabe
void-57 <ruddhaani60@gmail.com>
2025-03-24 23:36:51 +0530

    Add background removal link to homepage and improve link styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import React from "react";
 const Home = () => {
   return (
     <div className="p-4">
-      <Link href="/crop" className="mr-4 text-blue-500 hover:underline">
+      <Link href="/crop" className="block mb-4 text-blue-500 hover:underline">
         Crop Image
       </Link>
-      <Link href="/resize" className="text-blue-500 hover:underline">
+      <Link href="/resize" className="block mb-4 text-blue-500 hover:underline">
         Resize Image
+      </Link>
+      <Link href="/background-removal" className="block mb-4 text-blue-500 hover:underline">
+        Background Removal
       </Link>
     </div>
   );
